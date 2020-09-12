1752b5c7144a3bcef3eab893df3779ab910d5d2b
Andrew Bastin <andrewbastin.k@gmail.com>
2020-09-12 10:52:25 -0400

    Added code for past components. Placeholder emotions, life and profile comps.

---
+++ components/past/PastLife.vue
@@ -0,0 +1,116 @@
+<template>
+  <div
+    class="flex flex-col mt-8 transition duration-150 ease-in-out border rounded-lg lg:p-4 border-divider bg-primaryLight"
+  >
+    <div class="inline-flex flex-col flex-1 p-4">
+      <div class="mb-4 text-xl font-bold text-secondaryDark">Past 7 days</div>
+      <div class="flex overflow-auto">
+        <table class="w-full">
+          <thead class="text-xs text-left uppercase">
+            <tr>
+              <th class="py-3 pr-6 border-b border-divider">Date</th>
+              <th class="px-6 py-3 border-b border-divider">Task</th>
+              <th class="px-6 py-3 border-b border-divider">Status</th>
+              <th class="px-6 py-3 border-b border-divider">Reward</th>
+            </tr>
+          </thead>
+          <tbody>
+            <EmptyTable v-if="past.length === 0" />
+            <EmptyTable v-if="past.length === 0" />
+            <tr v-for="(day, index) in past" v-else :key="index">
+              <td class="py-4 pr-6 whitespace-no-wrap border-b border-divider">
+                <div class="flex items-center">
+                  <div class="truncate">
+                    <span class="font-bold text-secondaryDark">
+                      {{ day.date }}
+                    </span>
+                    <div class="text-sm truncate">{{ day.mood }}</div>
+                  </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-divider">
+                <div class="font-bold text-secondaryDark">{{ day.task }}</div>
+                <div class="text-xs">{{ day.duration }}</div>
+              </td>
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-divider">
+                <ChipPrimary
+                  :label="day.status"
+                  :color="getStatusColor(day.status)"
+                />
+              </td>
+              <td
+                class="px-6 py-4 font-bold whitespace-no-wrap border-b text-secondaryDark border-divider"
+              >
+                {{ day.reward }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moods from '~/helpers/data/moods'
+import rewards from '~/helpers/data/rewards'
+import tasks from '~/helpers/data/tasks'
+import * as fb from '~/helpers/firebase'
+import { getShortDateString } from '~/helpers/utils/dateutils'
+
+export default {
+  data() {
+    return {
+      past: [],
+      cancel7DaysSubscription: null,
+    }
+  },
+  mounted() {
+    const sub = fb.subscribeToLogEntriesForPast7Days(
+      this.$fireAuth,
+      this.$fireStore,
+      (logs) => {
+        this.past = logs
+          .map((log) => {
+            return {
+              date: getShortDateString(log.date),
+              mood: moods[log.activeMood].name,
+              task: tasks[log.activeTask].name,
+              duration: '20 mins',
+              status: this.getTaskStatusMessage(log.taskStatus),
+              reward: rewards[log.activeReward].name,
+            }
+          })
+          .reverse()
+      }
+    )
+    this.cancel7DaysSubscription = sub
+  },
+  beforeDestroy() {
+    if (this.cancel7DaysSubscription) this.cancel7DaysSubscription()
+  },
+  methods: {
+    getTaskStatusMessage(logTaskStatus) {
+      const messages = {
+        PENDING: 'Not reported',
+        COMPLETED: 'Completed',
+        // TODO : 'NOT-COMPLETE': 'Not completed',
+        CANCELLED: 'Cancelled',
+      }
+      return messages[logTaskStatus] || logTaskStatus
+    },
+    getStatusColor(status) {
+      switch (status) {
+        case 'Completed':
+          return 'green'
+        case 'Not reported':
+          return 'orange'
+        case 'Cancelled':
+          return 'red'
+        default:
+          break
+      }
+    },
+  },
+}
+</script>
