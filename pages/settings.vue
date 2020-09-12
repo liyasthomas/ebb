e62ed383c232aea0fdcab0bec681d5887fb68c73
Liyas Thomas <liyascthomas@gmail.com>
2020-09-12 14:58:23 +0530

    refactor: settings page with general and account tabs - resolved #5

---
+++ pages/settings.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col p-4 lg:flex-row">
+    <SidebarLeft>
+      <div
+        class="hidden px-4 py-8 text-sm font-bold tracking-wide lg:inline-flex text-secondaryLight"
+      >
+        settings
+      </div>
+      <TabPrimary to="/settings" label="General" icon="settings" exact />
+      <TabPrimary
+        to="/settings/account"
+        label="Account"
+        icon="account_circle"
+        exact
+      />
+    </SidebarLeft>
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth-redirect-signin',
+}
+</script>
