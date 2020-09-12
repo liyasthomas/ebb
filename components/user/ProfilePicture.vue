e2d007a252c325ab104f676576342d80b4b960e7
Liyas Thomas <liyascthomas@gmail.com>
2020-09-12 15:13:10 +0530

    WIP: initial profile page, profile picture component

---
+++ components/user/ProfilePicture.vue
@@ -0,0 +1,46 @@
+<template>
+  <img
+    class="shadow-inner bg-primaryDark"
+    :class="`h-${size} w-${size} rounded-${radius} bg-${color} object-cover`"
+    :src="url"
+    :alt="alt"
+    loading="lazy"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      default: `https://avatars.dicebear.com/v2/avataaars/${Math.random()
+        .toString(36)
+        .substring(7)}.svg?mood[]=happy`,
+    },
+    size: {
+      type: Number,
+      default: 6,
+    },
+    radius: {
+      type: String,
+      default: 'full',
+    },
+    color: {
+      type: String,
+      default: 'gray',
+    },
+    alt: {
+      type: String,
+      default: 'Profile picture',
+      // default() {
+      //   return this.url
+      //     .split('/')
+      //     .pop()
+      //     .split('.')[0]
+      //     .split('#')[0]
+      //     .split('?')[0]
+      // },
+    },
+  },
+}
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col p-4 lg:flex-row">
+    <SidebarLeft> </SidebarLeft>
+    <div class="flex flex-col flex-1">
+      <div class="inline-flex flex-col flex-1 h-full lg:flex-row">
+        <div class="inline-flex flex-col flex-1 p-4">
+          <TranslateGroupDown>
+            <div
+              key="profile"
+              class="flex flex-col items-center justify-center p-4 mb-4"
+            >
+              <ProfilePicture
+                :src="this.$store.state.authUser.photoURL"
+                :size="32"
+                class="border-8 border-accent"
+              />
+              <h1 class="mt-4 text-2xl font-bold text-secondaryDark">
+                {{ this.$store.state.authUser.displayName }}
+              </h1>
+            </div>
+            <TodayTasks key="today" />
+            <PastTasks key="past" />
+            <Empty key="empty" />
+          </TranslateGroupDown>
+        </div>
+        <SidebarRight> </SidebarRight>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth-redirect-signin',
+}
+</script>
