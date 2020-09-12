3d58699944052a6fef60348607a134e69471c5e8
Liyas Thomas <liyascthomas@gmail.com>
2020-09-12 20:33:02 +0530

    feat: empty state placeholder components

---
+++ components/ui/Empty.vue
@@ -0,0 +1,32 @@
+<template>
+  <TranslateGroupDown
+    class="flex flex-col items-center justify-center flex-1 max-w-sm px-8 py-4 mx-auto text-center"
+  >
+    <Lottie
+      key="animation"
+      :height="'260px'"
+      :options="emptyOptions"
+      :controls="false"
+      :loop="false"
+    />
+    <div key="body" class="flex flex-col items-center justify-center w-full">
+      <h3 class="mb-4 text-lg font-bold">
+        It's empty here.
+        <br />
+        Add your emotion.
+      </h3>
+      <ButtonPrimary to="/start" icon="keyboard_arrow_right" label="Start" />
+    </div>
+  </TranslateGroupDown>
+</template>
+<script>
+import emptyAnimation from '~/static/animations/empty.json'
+
+export default {
+  data() {
+    return {
+      emptyOptions: { animationData: emptyAnimation },
+    }
+  },
+}
+</script>
---
+++ components/ui/EmptyTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <tr class="animate-pulse">
+    <td class="py-4 pr-6 whitespace-no-wrap border-b border-divider">
+      <div class="flex items-center">
+        <div class="truncate">
+          <div class="w-16 h-4 mb-2 rounded bg-secondary"></div>
+          <div class="w-8 h-3 rounded bg-secondaryLight"></div>
+        </div>
+      </div>
+    </td>
+    <td class="px-6 py-4 whitespace-no-wrap border-b border-divider">
+      <div class="w-16 h-4 mb-2 rounded bg-secondary"></div>
+      <div class="w-6 h-2 rounded bg-secondaryLight"></div>
+    </td>
+    <td class="px-6 py-4 whitespace-no-wrap border-b border-divider">
+      <div class="w-16 h-4 rounded-lg bg-secondaryLight"></div>
+    </td>
+    <td
+      class="px-6 py-4 font-bold whitespace-no-wrap border-b text-secondary border-divider"
+    >
+      <div class="w-16 h-4 rounded bg-secondaryLight"></div>
+    </td>
+  </tr>
+</template>
