72d23e6f30062ff013a7313d6fd0f6e5c8ea1fac
Liyas Thomas <liyascthomas@gmail.com>
2020-09-12 17:18:29 +0530

    WIP: initial emotion prompt component

---
+++ components/card/SelectCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="inline-flex items-center p-4 transition duration-150 ease-in-out border rounded-lg cursor-pointer md:justify-center md:flex-col border-divider hover:border-dividerDark focus:border-dividerDark bg-primaryLight hover:text-secondaryDark focus:outline-none"
+    :class="{ 'bg-accentLight text-secondaryDark': active }"
+  >
+    <Lottie
+      key="animation"
+      :options="options"
+      :controls="false"
+      class="w-12 h-12 mr-2 md:w-full md:h-full md:order-2 md:mr-0"
+    />
+    <span class="text-sm font-bold md:mb-2 md:order-1">{{ name }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String, default: null },
+    active: { type: Boolean, default: false },
+    options: { type: Object, default: () => {} },
+  },
+}
+</script>
---
+++ components/ui/EmotionPrompt.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="inline-flex flex-col flex-1 h-full lg:flex-row">
+    <div class="inline-flex flex-col flex-1 p-4">
+      <TranslateDown>
+        <div
+          class="flex flex-col lg:p-4 transition duration-150 ease-in-out border-2 border-dashed rounded-lg border-divider bg-primaryLight"
+        >
+          <div class="inline-flex flex-col flex-1 p-4">
+            <div class="text-2xl font-bold text-secondaryDark">
+              {{ greet }} {{ this.$store.state.authUser.displayName }},
+              <br />
+              How are you feeling today?
+            </div>
+          </div>
+          <div class="inline-flex flex-col flex-1 p-4 md:flex-row">
+            <ButtonPrimary
+              to="/start"
+              icon="keyboard_arrow_right"
+              label="Start"
+            />
+          </div>
+        </div>
+      </TranslateDown>
+    </div>
+    <SidebarRight> </SidebarRight>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greet: this.getGreeting(),
+    }
+  },
+  methods: {
+    getGreeting() {
+      const hrs = new Date().getHours()
+      let msg = 'Welcome'
+      // if (hrs > 0) msg = "Mornin' Sunshine!"
+      if (hrs > 6) msg = 'Good morning'
+      if (hrs > 12) msg = 'Good afternoon'
+      if (hrs > 17) msg = 'Good evening'
+      // if (hrs > 22) msg = 'Go to bed!'
+      return msg
+    },
+  },
+}
+</script>
