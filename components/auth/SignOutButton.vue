9e4dfd467357c544b1be0689008cd46d67f0fb07
Liyas Thomas <liyascthomas@gmail.com>
2020-09-12 13:30:46 +0530

    feat: sign out modal and button - resolved #12

---
+++ components/auth/SignOutButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <ButtonPrimary label="Sign out" color="red" @click.native="signOut" />
+</template>
+
+<script>
+export default {
+  methods: {
+    async signOut() {
+      try {
+        await this.$fireAuth.signOut()
+        this.$toast.info('Signed out', {
+          icon: 'person',
+        })
+        this.$router.push('/')
+      } catch (e) {
+        alert(e)
+        this.$toast.error('Error while signing out', {
+          icon: 'error',
+        })
+      }
+    },
+  },
+}
+</script>
---
+++ components/modal/SignOutModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <modal
+    name="sign-out"
+    height="auto"
+    width="300"
+    classes="p-8 rounded-lg shadow-lg bg-primaryLight"
+  >
+    <h2 class="text-xl font-bold text-secondaryDark">Sign out</h2>
+    <p class="mt-4">
+      You can always log back in at any time. If you just want to log in to
+      another device, you can do that from the
+      <nuxt-link
+        :to="localePath('/life')"
+        class="transition duration-150 ease-in-out border-b-2 text-accent hover:border-accent focus:outline-none"
+        @click.native="$modal.hide('sign-out')"
+      >
+        ebb life
+      </nuxt-link>
+      .
+    </p>
+    <div class="flex mt-4">
+      <SignOutButton v-close-popover @click.native="$modal.hide('sign-out')" />
+      <ButtonSecondary
+        label="Cancel"
+        color="gray"
+        class="ml-2"
+        @click.native="$modal.hide('sign-out')"
+      />
+    </div>
+  </modal>
+</template>
