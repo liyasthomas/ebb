e0df95b05ca12a2afb6c69bc1466c1fd2246335c
Liyas Thomas <liyascthomas@gmail.com>
2020-09-12 14:11:02 +0530

    refactor: language chooser for i18n

---
+++ components/ui/ChangeLanguage.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-popover>
+    <ButtonSecondary icon="translate" :label="$i18n.locale.toUpperCase()" />
+    <template slot="popover">
+      <nuxt-link
+        v-for="locale in $i18n.locales.filter(
+          ({ code }) => code !== $i18n.locale
+        )"
+        :key="locale.code"
+        :to="switchLocalePath(locale.code)"
+      >
+        <PopoverItem v-close-popover :label="locale.name" />
+      </nuxt-link>
+    </template>
+  </v-popover>
+</template>
